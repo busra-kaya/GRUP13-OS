589ac083748b70b7193cceb72b739a9d0c917dff
tunahansahinn <82779761+tunahansahinn@users.noreply.github.com>
2024-12-16 17:08:39 +0300

    Pipe ile bağlanmış komutların ayrıştırılması için işlev eklendi.

---
+++ pipeparser.c
@@ -0,0 +1,32 @@
+/*Ali Osman Öcalır - B221210404 – 1. Öğretim B Grubu
+
+Zeynep İrem Tekin - B191210047 - 1. Öğretim B Grubu
+
+Tunahan Şahin - G201210023 - 2. Öğretim B Grubu
+
+Dursun Özer - B231210301 - 1. Öğretim A Grubu
+
+Büşra Kaya – B201210045 – 1. Öğretim C Grubu
+*/
+
+#include "pipeparser.h"
+#include <string.h>
+#include <stdbool.h>
+
+// Kullanıcı girişini analiz ederek pipe ile bağlanmış komutları ayrıştırır.
+// giris: Kullanıcıdan alınan tam komut metni.
+// komutlar: Ayrıştırılmış komutların tutulacağı dizi.
+// komut_sayisi: Ayrıştırılan toplam komut sayısını döndürmek için bir işaretçi.
+void pipe_komut_ayristir(char* giris, char* komutlar[], int* komut_sayisi) {
+    char* bolum = strtok(giris, "|");
+
+    // Başlangıç değerlerini sıfırla
+    *komut_sayisi = 0;
+
+    // Pipe ile ayrılmış komutları ayrıştır
+    while (bolum != NULL && *komut_sayisi < 10) {
+        komutlar[(*komut_sayisi)++] = bolum;
+        bolum = strtok(NULL, "|");
+    }
+}
+
